5f5edcda2ca0132caf7c5658b49e8ec66e988289
Jenna <jenna.fauconnier@gmail.com>
2022-10-11 18:45:06 +0200

    last update

---
+++ webapp/src/components/PlayerControls.jsx
@@ -0,0 +1,133 @@
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import { BsFillPlayCircleFill, BsFillPauseCircleFill, BsShuffle } from 'react-icons/bs'
+import { CgPlayTrackNext, CgPlayTrackPrev } from 'react-icons/cg'
+import { FiRepeat } from 'react-icons/fi'
+import { useStateProvider } from '../utils/StateProvider'
+import axios from "axios"
+import { reducerCases } from '../utils/Constants'
+
+export default function PlayerControls() {
+    const [{token, playerState}, dispatch] = useStateProvider()
+
+    const changeTrack = async (type) => {
+            await axios.post(`https://api.spotify.com/v1/me/player/${type}`, {} , {
+                headers: {
+                    Authorization: "Bearer " + token,
+                    "Content-Type": "application/json"
+                },
+            })
+            dispatch({ type:reducerCases.SET_PLAYER_STATE, playerState: true  })
+            const res = await axios.get(
+                "https://api.spotify.com/v1/me/player/currently-playing",
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: "Bearer " + token,
+                    },
+                }
+            );
+
+            if (res.data !== "") {
+                const currentPlaying = {
+                    id: res.data.item.id,
+                    name: res.data.item.name,
+                    artists: res.data.item.artists.map((artist) => artist.name),
+                    image: res.data.item.album.images[2].url,
+                };
+                dispatch({ type: reducerCases.SET_PLAYING, currentPlaying });
+            } else {
+                dispatch({ type: reducerCases.SET_PLAYING, currentPlaying: null });
+            }
+    }
+
+
+    const changeState = async () => {
+        const state = playerState ? "pause" : "play"
+
+        const res = await axios.put(
+            `https://api.spotify.com/v1/me/player/${state}`, {},
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + token,
+                },
+            }
+        );
+        dispatch({ type: reducerCases.SET_PLAYER_STATE, playerState: !playerState });
+    }
+
+
+
+    // useEffect(() => {
+    //     const getCurrentTrack = async () => {
+    //         const response = await axios.get('https://api.spotify.com/v1/me/player/currently-playing', {
+    //             headers: {
+    //                 Authorization: "Bearer " + token,
+    //                 "Content-Type": "application/json"
+    //             },
+    //         })
+    //         if(response?.data !== "") {
+    //             const { item } = response.data
+    //             const currentlyPlaying = {
+    //                 id: item.id,
+    //                 name: item.name,
+    //                 artists: item.artists.map((artist) => artist.name),
+    //                 image: item.album.images[2].url,
+    //             }
+    //             console.log('HERE : ', currentlyPlaying);
+
+    //             dispatch({ type:reducerCases.SET_PLAYING, currentlyPlaying })
+    //         }
+    //     }
+    //     getCurrentTrack()
+    // }, [token, dispatch])
+
+  return (
+    <Container>
+        <div className="shuffle">
+            <BsShuffle />
+        </div>
+        <div className="previous">
+            <CgPlayTrackPrev onClick={ () => changeTrack('previous') } />
+        </div>
+        <div className="state">
+            {playerState ? <BsFillPauseCircleFill onClick={ () => changeState('pause')} /> : <BsFillPlayCircleFill onClick={ () => changeState('play')} />}
+        </div>
+        <div className="next">
+            <CgPlayTrackNext onClick={ () => changeTrack('next') } />
+        </div>
+        <div className="repeat">
+            <FiRepeat />
+        </div>
+    </Container>
+  )
+}
+
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+
+    svg {
+        color: #b3b3b3;
+        transition: 0.2s ease-in-out;
+        &:hover {
+            color: white;
+        }
+    }
+
+    .state {
+        svg {
+            color: white;
+        }
+    }
+
+    .previous,
+    .next,
+    .state {
+        font-size: 2rem;
+    }
+`
